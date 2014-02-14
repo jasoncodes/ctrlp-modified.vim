c6e2c699b0826fd1a68cf59a86c1ef61e61f7668
Aldis Berjoza <graudeejs@yandex.ru>
2014-02-14 23:01:44 +0200

    Fix bashism

--- lib/branch.sh
+++ lib/branch.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -e
+#!/bin/sh -e
 FILES="$(git diff $(git merge-base origin/HEAD HEAD).. --name-only)"
-if [[ -n "$FILES" ]]; then
+if [ -n "$FILES" ]; then
   echo "$FILES" | sort -u | xargs find 2> /dev/null
 fi
