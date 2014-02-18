13c9be84fb780ea7dcca9d05c6d75db5fc78ca38
Jason Weathered <jason@jasoncodes.com>
2014-02-18 16:34:54 +1000

    Limit to unmerged files when merging inside a rebase

--- lib/modified.sh
+++ lib/modified.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-if [ -e .git/MERGE_HEAD ] || [ -d .git/rebase-merge ] || [ -e .git/CHERRY_PICK_HEAD ]; then
+if [ -e .git/MERGE_HEAD ] || [ -d .git/rebase-merge ] || [ -d .git/rebase-apply ] || [ -e .git/CHERRY_PICK_HEAD ]; then
   git status --porcelain -z | tr '\0' '\n' | grep '^[^?][^ ?] ' | cut -d ' ' -f 2-
 else
   FILES="$(git status --porcelain -z --untracked-files=all | tr '\0' '\n')"
