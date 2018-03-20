4929bda9e011e821402e7cad82d0953dc88e013d
Lisa Ugray <lisa.ugray@shopify.com>
2018-03-20 14:45:22 -0400

    Recover gracefully from unknown origin/HEAD

--- lib/branch.sh
+++ lib/branch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-FILES="$(git diff $(git merge-base origin/HEAD HEAD).. --name-only)"
+FILES="$(git diff $(git merge-base origin/HEAD HEAD 2> /dev/null || (git remote set-head origin -a > /dev/null && git merge-base origin/HEAD HEAD)).. --name-only)"
 if [ -n "$FILES" ]; then
   echo "$FILES" | sort -u | xargs find 2> /dev/null
 fi
