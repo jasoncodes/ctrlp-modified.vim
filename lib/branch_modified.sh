a0a88f7de1c6cb864c441411cc4aee942c04ed21
Jason Weathered <jason@jasoncodes.com>
2018-04-23 11:25:31 +1000

    Sort top-level files to the bottom of the match list

--- lib/branch_modified.sh
+++ lib/branch_modified.sh
@@ -1,2 +1,9 @@
 #!/bin/sh -e
-("$(dirname "$0")/branch.sh" && "$(dirname "$0")/modified.sh") | sort -u
+
+TEMP_FILE="$(mktemp "${TMPDIR:-/tmp}/ctrlp-modified.XXXXXX")"
+trap '{ rm -f "$TEMP_FILE"; }' EXIT
+
+("$(dirname "$0")/branch.sh" && "$(dirname "$0")/modified.sh") | sort -u > "$TEMP_FILE"
+
+grep / "$TEMP_FILE" || true
+grep -v / "$TEMP_FILE" || true
